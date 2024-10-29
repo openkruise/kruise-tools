a1f4d247e275e308239bfbc1b8a6675a9c583c91
Ai Ranthem <airanthem666@gmail.com>
2024-10-29 17:16:32 +0800

    support both rollout api v1beta1 and v1alpha1 (#105)
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -19,7 +19,8 @@ package api
 import (
 	kruiseappsv1alpha1 "github.com/openkruise/kruise-api/apps/v1alpha1"
 	kruiseappsv1beta1 "github.com/openkruise/kruise-api/apps/v1beta1"
-	kruiserolloutsv1apha1 "github.com/openkruise/kruise-rollout-api/rollouts/v1beta1"
+	kruiserolloutsv1alpha1 "github.com/openkruise/kruise-rollout-api/rollouts/v1alpha1"
+	kruiserolloutsv1beta1 "github.com/openkruise/kruise-rollout-api/rollouts/v1beta1"
 	apps "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -39,7 +40,8 @@ func init() {
 	_ = clientgoscheme.AddToScheme(Scheme)
 	_ = kruiseappsv1alpha1.AddToScheme(Scheme)
 	_ = kruiseappsv1beta1.AddToScheme(Scheme)
-	_ = kruiserolloutsv1apha1.AddToScheme(Scheme)
+	_ = kruiserolloutsv1alpha1.AddToScheme(Scheme)
+	_ = kruiserolloutsv1beta1.AddToScheme(Scheme)
 }
 
 func GetScheme() *runtime.Scheme {
