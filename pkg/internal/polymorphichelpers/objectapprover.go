355ed0a11a6883d3a5823ef50997848c03bb28dd
Ai Ranthem <zhongtianyun.zty@alibaba-inc.com>
2025-02-07 13:01:23 +0800

    can approve blue-green release (#115)
    Signed-off-by: AiRanthem <zhongtianyun.zty@alibaba-inc.com>

--- pkg/internal/polymorphichelpers/objectapprover.go
+++ pkg/internal/polymorphichelpers/objectapprover.go
@@ -36,10 +36,20 @@ func defaultObjectApprover(obj runtime.Object) ([]byte, error) {
 		obj.Status.CanaryStatus.CurrentStepState = rolloutsapiv1alpha1.CanaryStepStateReady
 		return runtime.Encode(scheme.Codecs.LegacyCodec(rolloutsapiv1alpha1.GroupVersion), obj)
 	case *rolloutsapiv1beta1.Rollout:
-		if obj.Status.CanaryStatus == nil || obj.Status.CanaryStatus.CurrentStepState != rolloutsapiv1beta1.CanaryStepStatePaused {
-			return nil, errors.New("does not allow to approve, because current canary state is not 'StepPaused'")
+		switch {
+		case obj.Status.CanaryStatus != nil:
+			if obj.Status.CanaryStatus.CurrentStepState != rolloutsapiv1beta1.CanaryStepStatePaused {
+				return nil, fmt.Errorf("does not allow to approve, because current canary state is not '%s'", rolloutsapiv1beta1.CanaryStepStatePaused)
+			}
+			obj.Status.CanaryStatus.CurrentStepState = rolloutsapiv1beta1.CanaryStepStateReady
+		case obj.Status.BlueGreenStatus != nil:
+			if obj.Status.BlueGreenStatus.CurrentStepState != rolloutsapiv1beta1.CanaryStepStatePaused {
+				return nil, fmt.Errorf("does not allow to approve, because current blue-green state is not '%s'", rolloutsapiv1beta1.CanaryStepStatePaused)
+			}
+			obj.Status.BlueGreenStatus.CurrentStepState = rolloutsapiv1beta1.CanaryStepStateReady
+		default:
+			return nil, fmt.Errorf("no need to approve: not in canary or blue-green progress")
 		}
-		obj.Status.CanaryStatus.CurrentStepState = rolloutsapiv1beta1.CanaryStepStateReady
 		return runtime.Encode(scheme.Codecs.LegacyCodec(rolloutsapiv1beta1.GroupVersion), obj)
 
 	default:
