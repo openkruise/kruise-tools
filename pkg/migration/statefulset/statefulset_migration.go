0ed8024a1ca8d2ea0871118c6d963b0dc7ccab7d
GautamBytes <manchandanigautam@gmail.com>
2025-07-02 18:42:02 +0000

    small tweak
    Signed-off-by: GautamBytes <manchandanigautam@gmail.com>

--- pkg/migration/statefulset/statefulset_migration.go
+++ pkg/migration/statefulset/statefulset_migration.go
@@ -106,7 +106,7 @@ func (c *control) Submit(src api.ResourceRef, dst api.ResourceRef, _ migration.O
 		return migration.Result{}, fmt.Errorf("orphan-delete SS: %w", err)
 	}
 
-	id := types.UID(uuid.NewUUID())
+	id := uuid.NewUUID()
 	result := migration.Result{ID: id, State: migration.MigrateSucceeded}
 	c.mu.Lock()
 	c.tasks[id] = &task{ID: id, start: time.Now(), src: src, dst: dst, result: result}
